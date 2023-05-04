7d7a4f2d425500e20ec0ee525eeaffe98f204a46
Zidane Innis <zidaneinnis@gmail.com>
2023-05-04 12:18:40 -0500

    Implementing an Authentication Flow

--- app.js
+++ app.js
@@ -57,18 +57,6 @@ app.use(errorController.get404);
 mongoose
   .connect(MONGODB_URI)
   .then((result) => {
-    User.findOne().then((user) => {
-      if (!user) {
-        const user = new User({
-          name: "Zidane",
-          email: "zidane@cluster1.com",
-          cart: {
-            items: [],
-          },
-        });
-        user.save();
-      }
-    });
     app.listen(3000);
   })
   .catch((err) => {
